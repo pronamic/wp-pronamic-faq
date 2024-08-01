a473879e4455187979bbe861e78a6f9c7cab5025
Karel-Jan Tolsma <>
2024-08-01 11:16:56 +0200

    Formatting. Short array syntax.

--- pronamic-faq.php
+++ pronamic-faq.php
@@ -29,52 +29,60 @@ function pronamic_faq_rewrite_flush() {
 
 register_activation_hook( __FILE__, 'pronamic_faq_rewrite_flush' );
 
-////////////////////////////////////////////////////////////
-
 /**
- * Register post type
+ * Register post type and taxonomy
  */
 function pronamic_faq_init() {
 	$rel_path = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
 
 	load_plugin_textdomain( 'pronamic_faq', false, $rel_path );
 
-	register_post_type( 'pronamic_faq', array(
-		'labels'             => array(
-			'name'               => _x( 'FAQ', 'post type general name', 'pronamic_faq' ), 
-			'singular_name'      => _x( 'FAQ', 'post type singular name', 'pronamic_faq' ), 
-			'add_new'            => _x( 'Add New', 'pronamic_faq', 'pronamic_faq' ),
-			'add_new_item'       => __( 'Add New FAQ', 'pronamic_faq' ),
-			'edit_item'          => __( 'Edit FAQ', 'pronamic_faq' ),
-			'new_item'           => __( 'New FAQ', 'pronamic_faq' ),
-			'view_item'          => __( 'View FAQ', 'pronamic_faq' ),
-			'search_items'       => __( 'Search FAQs', 'pronamic_faq' ),
-			'not_found'          => __( 'No FAQs found', 'pronamic_faq' ),
-			'not_found_in_trash' => __( 'No FAQs found in Trash', 'pronamic_faq' ), 
-			'parent_item_colon'  => __( 'Parent FAQ:', 'pronamic_faq' ),
-			'menu_name'          => __( 'FAQ', 'pronamic_faq' ),
-		),
-		'public'             => true,
-		'publicly_queryable' => true,
-		'show_ui'            => true,
-		'show_in_menu'       => true,
-		'query_var'          => true,
-		'show_in_rest'       => true,
-		'capability_type'    => 'post',
-		'has_archive'        => true,
-		'rewrite'            => array(
-			'slug'       => 'faq', 
-			'with_front' => false
-		) ,
-		'menu_icon'          => 'dashicons-format-chat',
-		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'custom-fields' ) 
-	) );
+	register_post_type(
+		'pronamic_faq',
+		[
+			'labels'             => [
+				'name'               => _x( 'FAQ', 'post type general name', 'pronamic_faq' ), 
+				'singular_name'      => _x( 'FAQ', 'post type singular name', 'pronamic_faq' ), 
+				'add_new'            => _x( 'Add New', 'pronamic_faq', 'pronamic_faq' ),
+				'add_new_item'       => __( 'Add New FAQ', 'pronamic_faq' ),
+				'edit_item'          => __( 'Edit FAQ', 'pronamic_faq' ),
+				'new_item'           => __( 'New FAQ', 'pronamic_faq' ),
+				'view_item'          => __( 'View FAQ', 'pronamic_faq' ),
+				'search_items'       => __( 'Search FAQs', 'pronamic_faq' ),
+				'not_found'          => __( 'No FAQs found', 'pronamic_faq' ),
+				'not_found_in_trash' => __( 'No FAQs found in Trash', 'pronamic_faq' ), 
+				'parent_item_colon'  => __( 'Parent FAQ:', 'pronamic_faq' ),
+				'menu_name'          => __( 'FAQ', 'pronamic_faq' ),
+			],
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'show_in_rest'       => true,
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'rewrite'            => [
+				'slug'       => 'faq', 
+				'with_front' => false
+			],
+			'menu_icon'          => 'dashicons-format-chat',
+			'supports'           => [
+				'title',
+				'editor',
+				'author',
+				'thumbnail',
+				'custom-fields',
+			],
+		]
+	);
 
-	/* Include the FAQ category taxonomy */
-	register_taxonomy( 'pronamic_faq_category', 'pronamic_faq', 
-		array( 
+	register_taxonomy(
+		'pronamic_faq_category',
+		'pronamic_faq', 
+		[ 
 			'hierarchical' => true , 
-			'labels'       => array(
+			'labels'       => [
 				'name'              => _x( 'FAQ Category', 'category general name', 'pronamic_faq' ),
 				'singular_name'     => _x( 'FAQ Category', 'category singular name', 'pronamic_faq' ),
 				'search_items'      => __( 'Search FAQ Categories', 'pronamic_faq' ),
@@ -86,23 +94,26 @@ function pronamic_faq_init() {
 				'add_new_item'      => __( 'Add New FAQ Category', 'pronamic_faq' ),
 				'new_item_name'     => __( 'New FAQ Category Name', 'pronamic_faq' ),
 				'menu_name'         => __( 'Categories', 'pronamic_faq' )
-			),
+			],
 			'show_ui'      => true,
-			'query_var'    => true
-		)
+			'query_var'    => true,
+		]
 	);
 }
 
 add_action( 'init', 'pronamic_faq_init' );
 
+/**
+ * Set columns
+ * 
+ * @param $columns
+ */
 function pronamic_faq_set_columns( $columns ) {
-	$new_columns = array();
+	$new_columns = [];
 
 	if ( isset( $columns['cb'] ) ) {
 		$new_columns['cb'] = $columns['cb'];
 	}
-
-	// $newColumns['thumbnail'] = __( 'Thumbnail', 'pronamic_companies' );
 
 	if ( isset( $columns['title'] ) ) {
 		$new_columns['title'] = __( 'Question', 'pronamic_faq' );
@@ -125,12 +136,18 @@ function pronamic_faq_set_columns( $columns ) {
 	return $new_columns;
 }
 
-add_filter('manage_edit-pronamic_faq_columns' , 'pronamic_faq_set_columns');
+add_filter( 'manage_edit-pronamic_faq_columns' , 'pronamic_faq_set_columns' );
 
+/**
+ * Custom columns
+ * 
+ * @param $column
+ * @param $post_id
+ */
 function pronamic_faq_custom_columns( $column, $post_id ) {
 	switch ( $column ) {
 		case 'pronamic_faq_categories':
-			$terms = get_the_term_list( $post_id, 'pronamic_faq_category' , '' , ', ' , '' );
+			$terms = get_the_term_list( $post_id, 'pronamic_faq_category', '', ', ', '' );
 
 			if ( is_string( $terms ) ) {
 				echo $terms;
@@ -142,4 +159,4 @@ function pronamic_faq_custom_columns( $column, $post_id ) {
 	}
 }
 
-add_action( 'manage_posts_custom_column' , 'pronamic_faq_custom_columns', 10, 2 );
+add_action( 'manage_posts_custom_column', 'pronamic_faq_custom_columns', 10, 2 );
